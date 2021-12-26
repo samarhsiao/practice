ab4fd3864939e25df539c17ebaae6920d28454a8
Samar <s99502042@gmail.com>
2021-12-26 17:52:13 +0800

    first commit

---
+++ jwt-login/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+app.use(express.json())
+const posts=[
+  { username: "Samar", 
+    age:29
+  },
+  {
+    username: "Lily",
+    age: 23
+  }
+]
+
+
+app.get('/post', authenticateToken, (req, res) => {
+  
+  res.json(posts.filter(post=>post.username === req.user.name))
+  
+})
+
+app.post('/login',(req,res)=>{
+  
+  const username = req.body.username
+  const user = {
+    name:username
+  }
+
+  const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+  res.json({
+    accessToken: accessToken
+  })
+})
+
+function authenticateToken(req,res,next){
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if(token==null)return res.sendStatus(401)
+
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+    if(err)return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+app.listen(3000)
